736b3b9999742c66911ee3188e3f4094a0a24cae
DaviWillian03 <101760093+DaviWillian03@users.noreply.github.com>
2022-09-09 09:11:23 -0300

    Add files via upload

---
+++ codigo/Laboratório 1/exercicio 2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace roteiro_1_dw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+Console.WriteLine("insira um numero inteiro");
+int primo= int.Parse(Console.ReadLine());
+int final= primo2(primo);
+if(final==0)
+    Console.WriteLine("numero primo");        
+else
+    Console.WriteLine("não é primo");
+
+int primo2(int primo){
+    int sup=0;
+    for(int i=2; i<primo; i++){
+        if(primo%i == 0)
+            sup++;
+}
+    return sup;
+}
+
+
+for(int i=1; i<1000; i++){
+    int b=0;
+    for(int j=1; j<i; j++)
+    {
+        int a=0;
+        a = divisor(i,j);
+        if(a>0)
+            b=soma(b,a);
+    }
+    if(b==i)
+        Console.WriteLine(b);
+}
+
+int soma(int x, int y)
+{
+    return (x+y);
+}
+
+int divisor( int n, int p)
+{
+    if(n%p==0)
+        return p;
+    else
+        return 0;
+}
+
+        }
+
+    }
+
+}
---
+++ codigo/Laboratório 1/exercicio 4.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace roteiro_1_dw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+     
+int [,]A = new int [5, 3]; 
+
+Console.WriteLine("Informe valores para a matriz A");
+LeMatriz(A); 
+imprimeMatriz(A);
+float colunaA = somaColuna(A, 0, A, 1, A, 2);
+
+
+float linhaABC = somalinha(A, 0, A, 1, A,2);
+float linhaDE = somalinha(A, 3, A , 4);
+Console.WriteLine("Coulunas somadas: {0}, {1} e {2}", colunaA);
+Console.WriteLine("A soma dos elementos das linhas foi respectivamente: {0}, {1}, {2}, {3} e {4}", linhaA, linhaB, linhaC, linhaD, linhaE);
+
+float somaColuna(int [,] A, int col)
+{ 
+float soma=0;
+for (int i = 0; i < A.GetLength(0); i++)       
+        soma+= A[i, col];
+return soma;
+}
+
+float somalinha(int [,] A, int linha)
+{ 
+float soma=0;
+for (int i = 0; i < A.GetLength(1); i++)       
+        soma+= A[linha, i];
+return soma;
+}
+
+void LeMatriz(int [,] Matriz) 
+{                   
+for (int i = 0; i < Matriz.GetLength(0); i++)
+    for (int j = 0; j < Matriz .GetLength(1); j++) {
+        Console.WriteLine("[{0}, {1}] = ", i, j);
+        Matriz[i, j] = int.Parse(Console.ReadLine()); 
+    }
+}
+
+void imprimeMatriz(int [,] A)
+{ 
+int i, j; 
+for (i = 0; i < A.GetLength(0); i++) 
+{
+    for (j = 0; j < A.GetLength(1); j++) 
+        Console.WriteLine( A[i, j] + "l") ;
+    Console.WriteLine();
+}
+}   
+
+        }
+
+    }
+
+}
