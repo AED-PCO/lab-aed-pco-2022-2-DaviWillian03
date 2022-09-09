dc56107daf21c0d87f0e234ac166e2552e46452e
DaviWillian03 <101760093+DaviWillian03@users.noreply.github.com>
2022-09-09 10:12:07 -0300

    Add files via upload

---
+++ codigo/Laboratório 2/exercicio 5 (vetor).cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+ int numeroLetras (string frase, char letra){
+        if (frase == "")
+            return (0);
+        else if (frase [0] == letra)
+            return numeroLetras (frase.Substring (1), letra) + 1;
+        else 
+            return numeroLetras (frase.Substring (1), letra);
+    }
+
+    string frase;
+    char letra;
+    Console.WriteLine("Informe a frase:");
+        frase = Console.ReadLine();
+        
+    Console.WriteLine("Informe a letra:");
+        letra=char.Parse(Console.ReadLine());
+
+    int n = numeroLetras(frase, letra);
+        Console.WriteLine("A frase {0} tem {1} letras {2}", frase, n, letra);
+
