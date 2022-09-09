b559348c23b5de79f0d3f23f7bee5dbfc73fb7be
DaviWillian03 <101760093+DaviWillian03@users.noreply.github.com>
2022-09-09 09:49:47 -0300

    Add files via upload

---
+++ codigo/Laboratório 2/exercicio 1 (fatorial).cs
@@ -0,0 +1,22 @@
+﻿//exercicio fatorial
+
+    int n, res=0;
+    Console.WriteLine("Insira um numero inteiro:");
+        n = int.Parse(Console.ReadLine());
+
+    int fatorial (int n){
+        if(n==2)
+            return(2);
+    int auxiliar = 0;
+        auxiliar=n*fatorial (n-1);
+            return (auxiliar);
+    }
+
+    res = fatorial(n);
+    Console.WriteLine("O fatorial de {0} é {1}", n, res);
+
+        }
+
+    }
+
+}
