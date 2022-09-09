2b6eb8dfd6499a99e7db0dc7e1f7448fd0bf47b0
DaviWillian03 <101760093+DaviWillian03@users.noreply.github.com>
2022-09-09 09:55:46 -0300

    Add files via upload

---
+++ codigo/Laboratório 2/exercicio 2 (multiplicação).cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+   static void Atividade2()
+    {
+        Console.WriteLine("\nEscreva um método recursivo que realize a multiplicação de doisnúmeros inteiros, A e B, recebidos por parâmetro.");
+
+        int valor1, valor2, result;
+
+        Console.WriteLine("\nInsira o primeiro valor desejado:");
+        valor1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("\nInsira o segundo valor desejado:");
+        valor2 = int.Parse(Console.ReadLine());
+
+        result = Multiplicacao(valor1,valor2);
+        Console.WriteLine("A multiplicação de {0} X {1} é: {2}", valor1, valor2, result);
+    }
+
+    static int Multiplicacao(int valor1,int valor2)
+    {
+        if (valor2 == 0)
+        {
+            return valor2;
+        }
+        else
+        {
+            return valor1 + Multiplicacao(valor1, valor2 - 1);
+        }
+    }
