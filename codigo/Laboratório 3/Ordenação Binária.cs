a8c01fa129f76df57baeed6d2d1813a19d07b65a
DaviWillian03 <101760093+DaviWillian03@users.noreply.github.com>
2022-10-13 09:13:29 -0300

    Update Ordenação Binária.cs

--- codigo/Laboratório 3/Ordenação Binária.cs
+++ codigo/Laboratório 3/Ordenação Binária.cs
@@ -1,1 +1,30 @@
- 
+ public static int[] insertionSort(int[] vetor)
+{
+
+    int i, j, atual;
+
+    for (i = 1; i < vetor.Length; i++)
+
+    {
+
+        atual = vetor[i];
+
+        j = i;
+
+        while ((j > 0) &amp;&amp; (vetor[j - 1] > atual))
+
+        {
+
+            vetor[j] = vetor[j - 1];
+
+            j = j - 1;
+
+        }
+
+        vetor[j] = atual;
+
+    }
+
+    return vetor;
+
+}
